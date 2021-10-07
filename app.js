cf7e2a7d5bdb062665b113d5dae59e24b6172ee4
Emily Summers <77934097+summerse99@users.noreply.github.com>
2021-10-07 13:10:22 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if (className == "light-theme") {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+
+    console.log('current class name: ' + className);
+
+});
+
+function welcomeStatement(evt, welcome) {
+
+    var welcome;
+
+    welcome = document.getElementsById("welcome");
+    evt.parentNode.removeChild(elem);
+    return false;
+
+}
+
+function openBee(evt, product) {
+
+
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    document.getElementById("welcome").innerHTML = "";
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(product).style.display = "block";
+    evt.currentTarget.className += " active";
+}
